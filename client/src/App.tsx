744943368aa7ca95425436f40eea775072e955b3
Paul Garton <paulgarton1984@hotmail.com>
2024-08-08 20:04:02 +0100

    create react app and add dependecies, bootstrap, routerand sass

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Container from "react-bootstrap/Container";
+
+function App() {
+  return (
+    <Container
+      style={{ border: "5px solid red", height: "500px", width: "1100px" }}
+    >
+      <h1>Battle Cards!</h1>
+    </Container>
+  );
+}
+
+export default App;
