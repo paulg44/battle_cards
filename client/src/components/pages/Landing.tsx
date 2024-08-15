c89ae5e04e4157c1331f5ff55ca3f45e1d8f919a
paulg44 <paulgarton1984@hotmail.com>
2024-08-15 18:25:34 +0100

    add username function

--- client/src/components/pages/Landing.tsx
+++ client/src/components/pages/Landing.tsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
 import "../../assets/css/landing.css";
 import Container from "react-bootstrap/Container";
 import { Link } from "react-router-dom";
 
 function Landing() {
+  const [username, setUsername] = useState("");
+
+  console.log(username);
+
   return (
     <div className="landingBackground">
       <Container className="landing">
         <h1>Battle Cards! Welcome to the arena! </h1>
+
+        <div className="signInOption">
+          <label>Enter Username or continue as guest</label>
+          <input onChange={(e) => setUsername(e.target.value)} />
+        </div>
 
         <div id="horror" className="horrorLandingCard">
           <h2>Horror</h2>
